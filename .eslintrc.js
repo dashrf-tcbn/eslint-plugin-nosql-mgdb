741616857fddba8a5fd3d0bcdc21c7c76fc42540
dashrf-ktsn <itsuout.tcbn@gmail.com>
2020-04-29 19:19:20 +0700

    eslint plugin to check for vulnerable in node.js + mongodb

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    "plugins": [
+        "nosql-mgdb"
+    ],
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+        "nosql-mgdb/use-orm": 2,
+        "nosql-mgdb/use-sanitizer": 2,
+        "nosql-mgdb/no-auth-function": 2,
+        "nosql-mgdb/no-where": 2,
+        "nosql-mgdb/no-exec": 2,
+        "nosql-mgdb/no-eval": 2,
+        "nosql-mgdb/no-settimeout": 2,
+        "nosql-mgdb/no-setinterval": 2,
+        "nosql-mgdb/no-function": 2,
+    },
+};
---
+++ eslint-plugin-nosql-mgdb/index.js
@@ -0,0 +1,133 @@
+/**
+ * @fileoverview no vanila mongoDB
+ * @author manita
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Rule Definition
+//------------------------------------------------------------------------------
+
+// used for checking if CallExpression node's calle.name is a mongoDB CRUD command
+let crudCommands = [
+    "insertOne",
+    "insertMany",
+    "find",
+    "findOne",
+    "updateOne",
+    "updateMany",
+    "replaceOne",
+    "deleteOne",
+    "deleteMany",
+    "bulkWrite"
+];
+
+module.exports.rules = {
+    "use-orm": context =>
+    ({ CallExpression: (node) =>
+        {
+            // check if it's a something like require('mongodb') or not
+            if (typeof node.callee !== 'undefined' && node.arguments.length != 0)  {
+                if ( node.callee.name == "require" && node.arguments[0].value == 'mongodb' ) {
+                    context.report(node, "Don't use vanilla mongodb, use ORM like sequelizejs or mongoose instead");
+                }
+            }
+        }
+    }),
+    "use-sanitizer" : context =>
+    ({ CallExpression: (node) =>
+        {
+            // check if it's a something like db.crudCommands.({username : String(req.body.username}) or not
+            if (typeof node.callee !== 'undefined' && node.arguments.length != 0) {
+                if (typeof node.callee.property !== 'undefined') {
+                    if (crudCommands.includes(node.callee.property.name) ) {
+                        node.arguments[0].properties.forEach((prop) => {
+                            if (!(prop.value.type === 'CallExpression')) {
+                                context.report(node, "mongodb crud arguments properties' value must be sanitized or use String()");
+                            }
+                        })
+                    }
+                }
+            }
+        }
+    }),
+    "no-auth-function" : context =>
+    ({ CallExpression: (node) =>
+        {
+            // check if it's a something like db.crudCommands.({ some prop }, auth_function) or not
+            if (typeof node.callee !== 'undefined' && node.arguments.length == 2) {
+                if (typeof node.callee.property !== 'undefined') {
+                    if (crudCommands.includes(node.callee.property.name) && (node.arguments[0].type == 'ObjectExpression' && node.arguments[1].type === 'Identifier')) {
+                        context.report(node, "Don't use auth function, it' vulnerable to Blind injections");
+                    }
+                }
+            }
+        }
+
+    }),
+    "no-where" : context =>
+    ({ Property: (node) =>
+        {
+            // check if is there a $where in .js file
+            if (node.key.name == "$where") {
+                    context.report(node, "Don't use mongodb $where");
+            }
+        }
+    }),
+    "no-exec": context =>
+    ({ CallExpression: (node) =>
+        {
+            // check if is there a exec() in .js file
+            if (typeof node.callee !== 'undefined') {
+                if (node.callee.name == "exec") {
+                    context.report(node, "Don't use exec() use execFile() instead");
+                }
+            }
+        }
+    }),
+    "no-eval": context =>
+    ({ CallExpression: (node) =>
+        {
+            // check if is there a eval() in .js file
+            if (typeof node.callee !== 'undefined') {
+                if (node.callee.name == "eval") {
+                    context.report(node, "Don't use eval()");
+                }
+            }
+        }
+    }),
+    "no-settimeout": context =>
+    ({ CallExpression: (node) =>
+        {
+            // check if is there a setTimeout() in .js file
+            if (typeof node.callee !== 'undefined') {
+                if (node.callee.name == "setTimeout") {
+                    context.report(node, "Don't use setTimeout()");
+                }
+            }
+        }
+    }),
+    "no-setinterval": context =>
+    ({ CallExpression: (node) =>
+        {
+            // check if is there a setInterval() in .js file
+            if (typeof node.callee !== 'undefined') {
+                if (node.callee.name == "setInterval") {
+                    context.report(node, "Don't use setInterval()");
+                }
+            }
+        }
+    }),
+    "no-function": context =>
+    ({ CallExpression: (node) =>
+        {
+            // check if is there a Function() in .js file
+            if (typeof node.callee !== 'undefined') {
+                if (node.callee.name == "Function") {
+                    context.report(node, "Don't use Function()");
+                }
+            }
+        }
+    }),
+}
+
